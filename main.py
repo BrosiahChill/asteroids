5368f82716fff4413a3c40e151070e6de54863df
BrosiahChill <josiahdeal93@gmail.com>
2025-05-15 23:57:30 -0500

    we have rotation and movement, also groups

--- main.py
+++ main.py
@@ -6,20 +6,28 @@ def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw(screen)
+
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
